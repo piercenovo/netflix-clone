c3bc91d51957014c1d4952a19d0f247c35008c8e
piercenovo <piercenovoah@gmail.com>
2023-03-11 00:18:15 -0500

    feat: add protecting routes

---
+++ src/hooks/useCurrentUser.ts
@@ -0,0 +1,9 @@
+import useSWR from 'swr'
+
+import fetcher from '@/../lib/fetcher'
+
+export function useCurrentUser () {
+  const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher)
+
+  return { data, error, isLoading, mutate }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
+import { NextPageContext } from 'next'
+import { getSession, signOut } from 'next-auth/react'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
+
+export async function getServerSideProps (context: NextPageContext) {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home () {
+  const { data: user } = useCurrentUser()
   return (
-    <>
-      <h1 className='text-white text-center text-2xl'>Netflix Clone</h1>
-    </>
+    <div className='flex flex-col gap-4 justify-center items-center w-full h-3/4'>
+      <h1 className='text-white text-2xl font-medium'>
+        Netflix Clone
+      </h1>
+      <p className='text-white mt-5'>Logged in as: {user?.name}</p>
+      <button className='h-10 w-36 bg-white rounded-md' onClick={() => signOut()}>
+        Logout!
+      </button>
+    </div>
   )
 }
